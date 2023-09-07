0a2af657c6363407bf6f9b6180f56e43fb7f9aca
Nielsen Pierce <NielsenPi@softcat.com>
2023-09-07 14:27:21 +0100

    Add dummy app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route for the root URL ("/") that responds with "Hello, World!"
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
